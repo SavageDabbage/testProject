044dcff12512d7639c824e39e9b7eb45a9b01214
Aadi K <aadiakaul@gmail.com>
2018-08-06 22:03:15 -0700

    First test class

---
+++ testprograms/src/main/java/com/example/testClass.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class testClass {
+    public static void main(String[] args) {
+        // Write your code here!
+        String firstName;
+        String lastName;
+
+        firstName = "Bob";
+        lastName = "Jones";
+
+        System.out.println(firstName);
+        System.out.print(lastName);
+
+
+    }
+}
